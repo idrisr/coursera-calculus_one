7008bf34eab55d09d548c3f5decdce84dcc50d0b
Idris Raja <idris.raja@gmail.com>
2014-10-03 11:43:24 -0400

    images and helper module Fx

---
+++ calculus/Fx.py
@@ -0,0 +1,35 @@
+#Series of helper functions I'm using over and over for the Coursera Calculus 1
+#class
+
+import matplotlib.pyplot as plt
+#TODO: Testing
+
+
+class Fx(object):
+    def __init__(self, f):
+        self.f = f
+        assert callable(self.f)
+
+    def limit_analysis(self, x):
+        # analyze value of function nearer and nearer to limit
+        self.X = self.limit_range(x, 8)
+        s = 'x=%0.10f y=%0.10f'
+        return '\n'.join([s % (x, self.f(x), ) for x in self.X])
+
+
+    @staticmethod
+    def limit_range(x, n=10):
+        """
+        x: limit value
+        n: number of points near limit to analyze. symmetrical + and - of x
+        """
+
+        X =      [x+10 ** -_ for _ in range(1, n)]
+        X.extend([x-10 ** -_ for _ in range(1, n)])
+        X = sorted(X)
+        return X
+
+    def show_plot(self, X):
+        Y = [self.f(x) for x in X]
+        plt.plot(X, Y)
+        plt.show()
